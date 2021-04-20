443ada796fa63a5284cd43d46d1278a95b11d99f
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-04-20 05:20:08 +0200

    ✨ Add tests setup

--- users/tests/test_home.py
+++ users/tests/test_home.py
@@ -1,2 +1,9 @@
-def test_home():
-    assert True
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.asyncio
+async def test_home(client: AsyncClient):
+    res = await client.get("/api/v1/home")
+    assert res.status_code == 200
+    assert res.json() == "Hello World!"
