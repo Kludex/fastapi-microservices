a9944a89b4059a6144434a314c67d74f70f85b5b
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-04-22 02:18:52 +0200

    ✨ Add dict() method on Base SQLAlchemy class

--- users/app/core/database.py
+++ users/app/core/database.py
@@ -1,4 +1,7 @@
+from typing import Any, Dict
+
 import humps
+from sqlalchemy import inspect
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import sessionmaker
@@ -16,3 +19,6 @@ class Base:
     @declared_attr
     def __tablename__(cls) -> str:
         return humps.depascalize(cls.__name__)
+
+    def dict(self) -> Dict[str, Any]:
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
