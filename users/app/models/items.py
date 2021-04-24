193d51686db7c96bb86b10b6731eee0958add957
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-04-24 20:26:20 +0200

    ✨ Move Base model to models package

--- users/app/models/items.py
+++ users/app/models/items.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-from app.core.database import Base
+from app.models.base import Base
 
 
 class Item(Base):
--- users/migrations/env.py
+++ users/migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import engine_from_config, pool
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from app import models  # noqa: F401
-from app.core.database import Base  # noqa: F401
+from app.models.base import Base  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
