8275913f9180592fdbb515b6c3aa2f2373a815e1
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-04-24 22:32:05 +0200

    ✅ Fix temporary test

--- users/tests/test_home.py
+++ users/tests/test_home.py
@@ -9,4 +9,3 @@ async def test_home(client: AsyncClient, superuser_token_headers: Dict[str, str]
     res = await client.get("/api/v1/home", headers=superuser_token_headers)
     assert res.status_code == 200
     assert res.json() == "Hello World!"
-    assert False
