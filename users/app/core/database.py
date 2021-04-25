36f340f5fc491dca33abba4a486c8821df4878a6
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-04-25 12:24:13 -0700

    ✨ Add probes (#41)

--- users/app/core/database.py
+++ users/app/core/database.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.POSTGRES_URI, echo=True)
+engine = create_async_engine(settings.POSTGRES_URI)
 SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
