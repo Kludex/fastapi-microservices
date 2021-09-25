4a89b254ed7ae023c2e3bf6421c9f5a0bc9e4408
Bastien Bouchoux <77078751+Bilibulle@users.noreply.github.com>
2021-09-25 22:08:09 +0200

    jsonable_encoder() is not working with many to many (#68)
    * Update base.py to handle complexe relations
    
    * Update base.py
    
    Update base.py to handle creation of obj if they have a field that is a too complexe relation
    
    * Update base.py
    
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- users/app/crud/base.py
+++ users/app/crud/base.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
 
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -17,7 +16,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     async def create(
         self, session: AsyncSession, obj_in: CreateSchemaType
     ) -> ModelType:
-        obj_in_data = jsonable_encoder(obj_in)
+        obj_in_data = dict(obj_in)
         db_obj = self._model(**obj_in_data)
         session.add(db_obj)
         await session.commit()
