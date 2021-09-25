ab8e2aaa999c22a4a2bb9b0a1e4796a3ed186fd8
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-09-25 22:06:39 +0200

    Update base.py

--- users/app/crud/base.py
+++ users/app/crud/base.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
 
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
