a16376cfb2bfe10459959cfe2cf57f19ace4b74b
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-04-03 19:16:03 +0200

    Update users/app/crud/users.py

--- users/app/crud/users.py
+++ users/app/crud/users.py
@@ -2,7 +2,5 @@ from app.crud.base import CRUDBase
 from app.models.users import User
 from app.schemas.user import UserInDB, UserUpdateDB
 
-class CRUDUser(CRUDBase[User, UserInDB, UserUpdateDB]):
-    pass
-
+CRUDUser = CRUDBase[User, UserInDB, UserUpdateDB]
 crud_user = CRUDUser(User)
